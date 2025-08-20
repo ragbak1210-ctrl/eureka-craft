a8cd88569229defbab6a1933bcadd73db19cb324
Raghav Bakshi <ragbak1210@gmail.com>
2025-08-21 00:26:32 +0530

    Update index.js

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,1 +1,9 @@
+// pages/admin/index.js
+export default function AdminPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold">Admin Dashboard (Coming Soon)</h1>
+    </div>
+  );
+}
 
