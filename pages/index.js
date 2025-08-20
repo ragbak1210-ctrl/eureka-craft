7167d8e51cec72cd7d423c9db66358c8ff772b15
Raghav Bakshi <ragbak1210@gmail.com>
2025-08-20 21:53:48 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">
+        🚀 Crafty Quiz Platform is Live!
+      </h1>
+    </div>
+  );
+}
