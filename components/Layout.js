e9ba3dfec4ddec382960b06ccec863efb12c685d
Raghav Bakshi <ragbak1210@gmail.com>
2025-08-23 16:55:17 +0530

    Create Layout.js

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+// components/Layout.js
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+}
