06260fd3ab50111ee1898f85b41020731cc8d016
Raghav Bakshi <ragbak1210@gmail.com>
2025-08-24 17:41:32 +0530

    Update seed-quizzes.js

--- seed-quizzes.js
+++ seed-quizzes.js
@@ -1,28 +1,24 @@
+require("dotenv").config({ path: ".env.local" });
+
 const admin = require("firebase-admin");
 const fs = require("fs");
 
-// Load the service account key JSON file
-const admin = require("firebase-admin");
-
-// Load from env var
+// Parse env var
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: serviceAccount.project_id,
-      clientEmail: serviceAccount.client_email,
-      privateKey: serviceAccount.private_key.replace(/\\n/g, "\n"),
-    }),
-  });
-}
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: serviceAccount.project_id,
+    clientEmail: serviceAccount.client_email,
+    privateKey: serviceAccount.private_key.replace(/\\n/g, "\n"),
+  }),
+});
 
 const db = admin.firestore();
 
 // Load quiz data
 const quizzes = JSON.parse(fs.readFileSync("./seed-quizzes.json", "utf8"));
 
-// Insert quizzes
 (async () => {
   try {
     for (const quiz of quizzes) {
