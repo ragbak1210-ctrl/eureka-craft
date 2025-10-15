493e5d45f966d23ab8d3d5f0df85f12f4944e529
Raghav Bakshi <ragbak1210@gmail.com>
2025-10-15 15:13:19 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#000000",
+        golden: "#c89b3c", // yellowish gold tone
+        yellow: "#fabe17",
+        white: "#fefefe",
+        graylight: "#f4f4f4",
+      },
+      fontFamily: {
+        primary: ["Raleway", "sans-serif"],
+        secondary: ["'Shadow Script'", "cursive"],
+      },
+      backgroundImage: {
+        "gold-gradient": "linear-gradient(90deg, #fabe17 0%, #c89b3c 100%)",
+      },
+    },
   },
   plugins: [],
-}
+};
