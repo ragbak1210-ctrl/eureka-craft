61ef1b7021e5c70f762b91de867de9018609b703
Raghav Bakshi <ragbak1210@gmail.com>
2025-10-23 13:31:03 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Layout from "@/components/Layout";
 import CraftyWidget from "@/components/CraftyWidget";
 import { Raleway, Shadows_Into_Light_Two } from "next/font/google";
 
+// ✅ Use only valid weights
 const raleway = Raleway({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -11,7 +12,7 @@ const raleway = Raleway({
 
 const shadows = Shadows_Into_Light_Two({
   subsets: ["latin"],
-  weight: "400", // ✅ fixed: only one available weight
+  weight: "400", // ✅ only available weight
   variable: "--font-shadows",
 });
 
@@ -20,7 +21,7 @@ export default function MyApp({ Component, pageProps }) {
     <div className={`${raleway.variable} ${shadows.variable} font-sans`}>
       <Layout>
         <Component {...pageProps} />
-        <CraftyWidget />
+        <CraftyWidget /> {/* Keep Crafty visible on all pages */}
       </Layout>
     </div>
   );
