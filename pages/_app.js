d7c056f4a273a399718931a122498cfc79764732
Raghav Bakshi <ragbak1210@gmail.com>
2025-10-23 13:14:17 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,17 @@ import Layout from "@/components/Layout";
 import CraftyWidget from "@/components/CraftyWidget";
 import { Raleway, Shadows_Into_Light_Two } from "next/font/google";
 
-const raleway = Raleway({ subsets: ["latin"], variable: "--font-raleway" });
-const shadow = Shadows_Into_Light_Two({ subsets: ["latin"], variable: "--font-shadow" });
+const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-raleway",
+});
+
+const shadowScript = Shadows_Into_Light_Two({
+  subsets: ["latin"],
+  weight: ["400"], // ✅ only 400 supported
+  variable: "--font-shadow",
+});
 
 export default function MyApp({ Component, pageProps }) {
   return (
