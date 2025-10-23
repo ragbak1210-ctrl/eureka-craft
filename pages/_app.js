a13d985e82858754039079b773f82e1505d9ba88
Raghav Bakshi <ragbak1210@gmail.com>
2025-10-23 12:57:43 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 import CraftyWidget from "@/components/CraftyWidget";
+import { Raleway, Shadows_Into_Light_Two } from "next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"], variable: "--font-raleway" });
+const shadow = Shadows_Into_Light_Two({ subsets: ["latin"], variable: "--font-shadow" });
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <CraftyWidget /> {/* Crafty everywhere */}
-    </Layout>
+    <div className={`${raleway.variable} ${shadow.variable} font-raleway bg-white text-black`}>
+      <Layout>
+        <Component {...pageProps} />
+        <CraftyWidget />
+      </Layout>
+    </div>
   );
 }
